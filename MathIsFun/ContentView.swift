866224257b5f55d7c8b4fd7d1d8b6b8fce2484fd
Karam Malkon <kmelkon@gmail.com>
2023-06-20 14:12:08 +0200

    feat: add settings view and sheet and gameview

--- MathIsFun/ContentView.swift
+++ MathIsFun/ContentView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct Question: Identifiable, Hashable {
+    let id = UUID()
+    let question: String
+    var answer: String
+}
+
 struct SettingsSheetView: View {
     var body: some View {
         VStack {
@@ -16,11 +22,73 @@ struct SettingsSheetView: View {
 }
 
 struct GameView: View {
-    @State var showingSettingsSheet = false
+    let numberOfQuestions: Int
+    let multiplicationTables: [Int]
     
+    @State private var showingSettingsSheet = false
+    @State private var questions: [Question]
+    @State private var currentQuestionIndex = 0
+    @State private var answer = ""
+    @State private var userAnswers: [String] = []
+    @State private var isAnswerCorrect = false
+    @State private var showNextButton = false
+    
+    init(numberOfQuestions: Int, multiplicationTables: [Int]) {
+        self.numberOfQuestions = numberOfQuestions
+        self.multiplicationTables = multiplicationTables
+        
+        var generatedQuestions: [Question] = []
+        
+        for _ in 0..<numberOfQuestions {
+            // TODO: don't generate the same question twice
+            let randomMultTable = multiplicationTables.randomElement() ?? 2
+            let randomMultiplier = Int.random(in: 1...12)
+            let randomQuestion = "\(randomMultiplier) X \(randomMultTable) = ?"
+            let answerToRandomQuestion = randomMultTable * randomMultiplier
+            
+            generatedQuestions.append(Question(question: randomQuestion, answer: "\(answerToRandomQuestion)"))
+        }
+        
+        self._questions = State(initialValue: generatedQuestions)
+    }
+
     var body: some View {
         VStack {
-            Text("GAME ON!")
+            if !questions.isEmpty && currentQuestionIndex < questions.count {
+                VStack {
+                    Text(questions[currentQuestionIndex].question)
+                        .font(.title)
+                        
+
+                    TextField("Enter your answer", text: $answer)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                    
+                    if showNextButton {
+                        Button("Next Question", action: moveToNextQuestion)
+                            .padding()
+                    } else {
+                        Button("Check My Answer") {
+                            print(questions)
+                            userAnswers.append(answer)
+                            print(userAnswers)
+                            if (answer == questions[currentQuestionIndex].answer) {
+                                isAnswerCorrect = true
+                                // Trigger alert or something to tell the user the result
+                            } else {
+                                isAnswerCorrect = false
+                            }
+                            showNextButton = true
+                            
+                        }
+                        .padding()
+                    }
+                }
+            } else {
+                Text("All questions answered!")
+                    .font(.title)
+                    .foregroundColor(.green)
+            }
         }
         .toolbar {
             Button("Restart") {
@@ -34,13 +102,22 @@ struct GameView: View {
         }
         
     }
+    
+    func moveToNextQuestion() {
+        answer = ""
+       if currentQuestionIndex < questions.count - 1 {
+           currentQuestionIndex += 1
+       }
+        showNextButton = false
+   }
 }
 
 struct ContentView: View {
     @State private var isGameActive = false
     
-    @State private var multiplicationTableChosen = 0
+    @State private var multiplicationTablesChosen = [Int]()
     @State private var gameLengthChosen = 5
+    @State private var difficulty = "easy"
     
     let gameLengthOptions = [5, 10, 20]
     let multiplicationTables = 2...12
@@ -49,6 +126,8 @@ struct ContentView: View {
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    
+    // TODO: add two game modes: multiple choice and fill in the blank
     
     var body: some View {
         NavigationView {
@@ -59,16 +138,15 @@ struct ContentView: View {
                 LazyVGrid(columns: multTableColumns, spacing: 20) {
                     ForEach(multiplicationTables, id: \.self) { tableNumber in
                         Button("\(tableNumber)") {
-                            // select mult table
-                            multiplicationTableChosen = tableNumber
+                            // TODO: prevent selecting the same table multiple times
+                            multiplicationTablesChosen.append(tableNumber)
+                            print(multiplicationTablesChosen)
                         }
                         .padding()
                         .frame(width: 60, height: 60)
                         .foregroundColor(.white)
                         .background(.mint)
                         .clipShape(RoundedRectangle(cornerRadius: 5))
-                        
-                        
                     }
                 }
                 .padding()
@@ -92,7 +170,7 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                NavigationLink(destination: GameView().onAppear{
+                NavigationLink(destination: GameView(numberOfQuestions: gameLengthChosen, multiplicationTables: multiplicationTablesChosen).onAppear{
                     // start the game
                     isGameActive = true
                 }) {
